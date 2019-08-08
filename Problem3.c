72cebd81e6a4697dc1703f9bd297b6046be3b45d
Odd_scholar <eduardo.oc99@hotmail.com>
2019-08-07 21:57:44 -0500

    Problem3 now works on big numbers

--- Problem3.c
+++ Problem3.c
@@ -2,57 +2,70 @@
 #include <stdio.h>
 
 // I define the constants
-#define SIZEARRAY 100
+#define ARRAY_SIZE 10000000
 
 // I define the variables
-unsigned int n = 13195;
+unsigned long long int n = 600851475143;
 int wait = 0;
-unsigned int ArrayPrimes[SIZEARRAY];
-unsigned int i = 2;
-int IsPrime = 1;
-unsigned int j;
-unsigned int k = 0;
-unsigned int l = 0;
-unsigned int product = 1;
+static unsigned long long int ArrayPrimes[ARRAY_SIZE] = {0};
+int IsPrime = 1; // This is a flag variable
+unsigned long long int LargestPrime = 0;
+unsigned long long int Guess = 3;
+
+// Loop variables
+unsigned long long int i;
 
 int main(){
+    // I set the first prime
+    ArrayPrimes[0] = 2;
 
-    for(j = 2; j < n; j++){
-            // This loop will see if a number is a prime
-            for(i = 2; i < j; i++){
-                if(ArrayPrimes[l] == 0){
-                    break;
-                }
-                if(j % ArrayPrimes[l] == 0){
-                    IsPrime = 0;
-                }
-                l++;
-            }
+    // I need an iterator to the Array
+    size_t NumberofPrimes = 1;
 
-            // If it is prime then it saves the value
-            if(IsPrime){
-                ArrayPrimes[k] = j;
-                k++;
+    // I have to test if it is divisible by the first prime
+    while(n % ArrayPrimes[NumberofPrimes - 1] == 0){
+        n = n / ArrayPrimes[NumberofPrimes - 1];
+    }
 
-                // I will make the n smaller
-                while((n / j) == 0){
-                    n = n / j;
-                }
+    // The main algorithm
+    while(1 < n){
 
-
+        // I find if the number is prime
+        for(i = 0; i < NumberofPrimes; i++){
+            if(Guess % ArrayPrimes[i] == 0){
+                IsPrime = 0;
+                break;
             } else{
                 ;
             }
+        }
 
-            // I change the flag to the original state
-            IsPrime = 1;
-            l = 0;
+        // I save the prime in the Array
+        if(IsPrime){
+            LargestPrime = Guess;
+            ArrayPrimes[NumberofPrimes] = LargestPrime;
+
+            // This will allow me to reduce the number on it's primes
+            while(n % ArrayPrimes[NumberofPrimes] == 0){
+                n = n / ArrayPrimes[NumberofPrimes];
+            }
+            NumberofPrimes++;
+        }
+
+        // I update the variables
+        Guess++;
+
+        // I return the flag to the original value
+        IsPrime = 1;
     }
 
+    LargestPrime = ArrayPrimes[NumberofPrimes - 1];
+
     // This is the output of the function
-    printf("%d", j);
+    printf("%d\n", LargestPrime);
 
     // This is the wait function
     scanf("%d", &wait);
+
     return 0;
 }
